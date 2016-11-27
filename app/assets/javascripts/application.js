cd113aac17011db34602a82cd09660ac81fba03f
Sandy <SVern99@aol.com>
2016-11-27 20:35:03 +0000

    added masonry-rails and jquery-turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@
 //= require turbolinks
 
 
-$('a[data-popup]').live("click", function(e) {
+$("a[data-popup]").live("click", function(e) {
     window.open( $(this).attr("href"), "Popup", "height=600, width=600" );
     e.preventDefault();
 });
